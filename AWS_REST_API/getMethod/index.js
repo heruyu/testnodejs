0b85de76cbccc8d3aad476bdd096b8a2d2e78dda
mvhertel <97692211+heruyu@users.noreply.github.com>
2023-02-21 00:44:27 +0100

    minor update getUser

--- AWS_REST_API/getMethod/index.js
+++ AWS_REST_API/getMethod/index.js
@@ -1,30 +1,35 @@
 import { DynamoDB } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
-const client = new DynamoDB({ region: "eu-north-1" });
+import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
+const client = new DynamoDB({region: 'eu-north-1'});
 const dynamo = DynamoDBDocumentClient.from(client);
 
 const getUser = async (event) => {
-  const user = {
-    TableName: "users",
-    KeyConditionExpression: "user_id = :i",
-    ExpressionAttributeValues: { ":i": event.pathParameters?.user_id },
-    ProjectionExpression: "name, age",
-  };
-  try {
-    const res = await dynamo.query(user).promise();
-
+  const path = event.pathParameters.user_id;
+  try{
+    const user = {
+      TableName: 'users',
+      KeyConditionExpression: '#db_user_id = :i',
+      ExpressionAttributeValues: { ':i': path },
+      ExpressionAttributeNames: { "#db_user_id": "user_id"},
+      ProjectionExpression: "users_name, age",
+      ScanIndexForward: true
+    }; 
+    
+    const res = await dynamo.send(new QueryCommand(user));
+    
     return {
-      statusCode: 201,
-      body: JSON.stringify(res.Items),
-    };
-  } catch (e) {
+        statusCode: 201,
+        body: JSON.stringify(res.Items),
+      };
+      
+  }catch(e){ 
+    
     return {
-      statusCode: 402,
-      headers: { "content-type": "application/json" },
-      body: JSON.stringify({
-        error: `no user with id: ${event.pathParameters?.user_id}`,
-      }),
-    };
+        statusCode: 402,
+        headers: {"content-type": "application/json"},
+        body: JSON.stringify({ error: `no user with id: ${path}`}), 
+      };
+      
   }
 };
 
